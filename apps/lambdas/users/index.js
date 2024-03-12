92f3d0f67ec3a5b93f6860670a2b09957f85599e
yzhbankov <yzhbankov@redpointpositioning.com>
2024-03-12 01:36:34 +0000

    Lambda function handle req parameters

--- apps/lambdas/users/index.js
+++ apps/lambdas/users/index.js
@@ -1,7 +1,10 @@
 import AWS from '@aws-sdk/client-athena';
+import { Repository } from './repository';
+import { getSqlByRequestParams } from './utils.js';
 
 // Create an Athena client
 const athena = new AWS.Athena();
+const repo = new Repository(athena);
 
 export const handler = async (event, context) => {
     const DB_NAME = process.env.DATABASE_NAME;
@@ -9,8 +12,11 @@ export const handler = async (event, context) => {
     const WORKGROUP = process.env.ATHENA_WORKGROUP;
     const OUTPUT_LOCATION = `s3://${process.env.ATHENA_OUTPUT_PATH}/`;
 
+    const idStartIndex = event.path.lastIndexOf('/') + 1;
+    const reqParam = event.path.substring(idStartIndex);
+
     // Set the Athena query string
-    const query = `SELECT * FROM "${TABLE_NAME}" where email like 'ihor%'`; // Replace with your actual database and table
+    const query = getSqlByRequestParams(reqParam, TABLE_NAME); // Replace with your actual database and table
 
     // Set the query execution context
     const queryParams = {
@@ -25,22 +31,10 @@ export const handler = async (event, context) => {
     };
 
     try {
-        // Execute the Athena query
-        const queryExecution = await athena.startQueryExecution(queryParams);
-
-        // Get the query execution ID
-        const queryExecutionId = queryExecution.QueryExecutionId;
-
-        // Wait for the query to complete
-        await waitForQuery(queryExecutionId);
-
-        // Get the query results
-        const queryResults = await getQueryResults(queryExecutionId);
-
-        // Return a successful response
+        const result = await repo.execute(queryParams);
         return {
             statusCode: 200,
-            body: JSON.stringify({ users: queryResults }),
+            body: JSON.stringify({ users: result }),
         };
     } catch (error) {
         // Log and return an error response
@@ -51,56 +45,3 @@ export const handler = async (event, context) => {
         };
     }
 };
-
-
-// Helper function to wait for Athena query completion
-async function waitForQuery(queryExecutionId) {
-    const params = {
-        QueryExecutionId: queryExecutionId,
-    };
-
-    while (true) {
-        const response = await athena.getQueryExecution(params);
-        const status = response.QueryExecution.Status.State;
-
-        if (status === 'SUCCEEDED' || status === 'FAILED' || status === 'CANCELLED') {
-            break;
-        }
-
-        // Wait for a few seconds before checking again
-        await new Promise((resolve) => setTimeout(resolve, 5000));
-    }
-}
-
-// Helper function to get Athena query results
-async function getQueryResults(queryExecutionId) {
-    const params = {
-        QueryExecutionId: queryExecutionId,
-    };
-
-    const response = await athena.getQueryResults(params);
-    const rows = response.ResultSet.Rows.map(row => row.Data.map((data) => data.VarCharValue));
-    const headerMap = convertHeaderListToJson(rows[0]);
-    return rows.reduce((memo, row, idx) => {
-        if (idx !== 0) {
-            memo.push(convertRowToObject(headerMap, row));
-        }
-        return memo;
-    }, []);
-}
-
-function convertHeaderListToJson(headerList) {
-    const result = {};
-    for (const headerIdx in headerList) {
-        result[headerIdx] = headerList[headerIdx];
-    }
-    return result;
-}
-
-function convertRowToObject(headersMap, row) {
-    const result = {};
-    for (const propIdx in row) {
-        result[headersMap[propIdx]] = row[propIdx];
-    }
-    return result;
-}
